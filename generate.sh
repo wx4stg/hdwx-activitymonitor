a38d0bd6f16150d9cbd0e5ae244df4ff5d03709f
Sam Gardner <stgardner4@tamu.edu>
2022-02-09 18:50:52 -0600

    add support for condaEnvName

--- generate.sh
+++ generate.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if [ -f ~/mambaforge/envs/HDWX/bin/python3 ]
+if [ -f ../config.txt ]
 then
-    ~/mambaforge/envs/HDWX/bin/python3 statusPlotter.py
+    source ../config.txt
+else
+    condaEnvName="HDWX"
 fi
-if [ -f ~/miniconda3/envs/HDWX/bin/python3 ]
+if [ -f ~/mambaforge/envs/$condaEnvName/bin/python3 ]
 then
-    ~/miniconda3/envs/HDWX/bin/python3 statusPlotter.py
+    ~/mambaforge/envs/$condaEnvName/bin/python3 statusPlotter.py
+fi
+if [ -f ~/miniconda3/envs/$condaEnvName/bin/python3 ]
+then
+    ~/miniconda3/envs/$condaEnvName/bin/python3 statusPlotter.py
 fi
